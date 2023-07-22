6c75ed4a36137a5114ed666fb05a3b237879dfdf
parham-json <pazargadiparham@gmail.com>
2023-07-22 13:57:21 +0430

    main.js done

---
+++ main.js
@@ -0,0 +1,10 @@
+function generatePassword(length=10, charset='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123459!@#$%^&*()_+~`|}{[]\:;?><,./-=') {
+    let password = '';
+    
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    
+    return password;
+}
